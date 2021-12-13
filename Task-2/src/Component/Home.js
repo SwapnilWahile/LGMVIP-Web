a314587414518e9b3a060dcacb5f7ed7e05ed382
Swapnil Wahile <swapnilwahile24@gmail.com>
2021-12-13 16:15:51 +0530

    Add files via upload

---
+++ Task-2/src/Component/Home.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './Home.css';
+
+export default function Home({ getData }) {
+    return (
+        <section className="home">
+            <div className="content">
+                <p><br/>Click the button in the<br/>navbar to get the users details.</p>
+            </div>
+        </section>
+    )
+}
